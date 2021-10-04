ba5263290e4c49cfa318a539b18e42da423fd258
Katelyn <katelynwest225@gmail.com>
2021-10-04 12:38:15 -0500

    Did 2D array exercises instructed by Lodis

---
+++ Tic-Tac-Toe/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                                          /*0, 1, 2*/
+            int[,] Array2D = new int[3, 3] { /*0*/ { 1, 2, 3 }, 
+                                             /*1*/ { 4, 5, 6 }, 
+                                             /*2*/ { 7, 8, 9 } };
+
+                                           /*0, 1, 2*/
+            int[,] Array2DExample2 = {/*0*/{ 1, 2, 3 }, 
+                                      /*1*/{ 4, 5, 6 }, 
+                                      /*2*/{ 7, 8, 9 }, 
+                                      /*3*/{10, 11, 12 } };
+
+            Add2DArray(Array2DExample2);
+
+            //Adding 2D array excercise
+            void Add2DArray(int[,] ArrayToAdd)
+            {
+                int FirstDimLength = ArrayToAdd.GetLength(0);
+                int SecDimLength = ArrayToAdd.GetLength(1);
+                int Result = 0;
+                for (int i = 0; i < FirstDimLength; i++)
+                {
+                    for (int j = 0; j < SecDimLength; j++)
+                    {
+                        Result += ArrayToAdd[i, j];
+                        if ((j + 1) >= SecDimLength)
+                        {
+                            Console.WriteLine(Result);
+                            Result = 0;
+                        }
+                    }
+                }
+            }
+
+            //Writing out Array Excercise
+            void WriteArrayExample1()
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.WriteLine(Array2D[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
